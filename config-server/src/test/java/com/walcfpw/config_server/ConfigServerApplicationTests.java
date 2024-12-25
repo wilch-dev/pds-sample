69a1f606779e8737531c6a57f68cc926904c32cd
wiliam.chan <walcfpw@gmail.com>
2024-12-25 17:29:37 +0800

    initial stuff

---
+++ config-server/src/test/java/com/walcfpw/config_server/ConfigServerApplicationTests.java
@@ -0,0 +1,15 @@
+package com.walcfpw.config_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.cloud.config.server.EnableConfigServer;
+
+@SpringBootTest
+@EnableConfigServer
+class ConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
